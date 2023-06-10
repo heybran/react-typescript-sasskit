1f278ca0218a61ec397ac8edd65ca952c9cbce9b
brandon <gwonzhang@gmail.com>
2023-06-10 21:26:50 +0800

    feat: add server.js to handle server fetch to github

---
+++ server.js
@@ -0,0 +1,49 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/api/auth/github/callback", async (req, res) => {
+  const { code } = req.body;
+
+  try {
+    const response = await fetch(
+      `https://github.com/login/oauth/access_token`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          client_id: process.env.GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          code,
+        }),
+      },
+    );
+
+    const data = await response.json();
+
+    if (data.error) {
+      throw new Error(data.error_description);
+    }
+
+    res.json({ access_token: data.access_token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
