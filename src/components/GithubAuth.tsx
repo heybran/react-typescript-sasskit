f728d19c2e9fd79085215f46c712421f9873c64e
brandon <gwonzhang@gmail.com>
2023-06-12 00:03:18 +0800

    cleanup: update GithubAuth components catch error bug

--- src/components/GithubAuth.tsx
+++ src/components/GithubAuth.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { redirect } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const GITHUB_CLIENT_ID = "0b3d7ed9ff20b068f060";
 const GITHUB_CALLBACK_URL = "http://localhost:5173/auth/github/callback";
@@ -14,28 +14,38 @@ const GitHubLoginButton = () => {
 };
 
 const GitHubAuth = () => {
-  useEffect(() => {
-    const code = new URLSearchParams(window.location.search).get("code");
-    if (!code) {
-      return console.error(`code is missing`);
-    }
+  const navigate = useNavigate();
 
-    fetch(`http://localhost:5000/api/auth/github/callback`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ code }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
+  useEffect(() => {
+    (async () => {
+      const code = new URLSearchParams(window.location.search).get("code");
+      if (!code) {
+        return console.error(`code is missing`);
+      }
+
+      const res = await fetch(
+        `${import.meta.env.VITE_REACT_APP_SERVER_URL}/api/auth/github/callback`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ code }),
+        },
+      );
+
+      if (res.status === 200) {
+        const data = await res.json();
         // Save the access token to local storage or a cookie
         localStorage.setItem("access_token", data.access_token);
-        redirect("/");
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+        // Why this code still runs after res.status(500)?
+        // that was because inside server.js, it didn't throw any error,
+        // res.status(500).json({ error: error.message });
+        navigate("/");
+      } else {
+        console.error("something wrong");
+      }
+    })();
   }, []);
 
   return <div>Authenticating...</div>;
