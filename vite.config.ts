e4843ccbc677892f08fc7328c282cd69ea5cb919
brandon <gwonzhang@gmail.com>
2023-06-13 15:27:44 +0800

    feat: add vite server proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@furnitures": "/src/assets/furnitures"
-    }
-  }
-})
+      "@furnitures": "/src/assets/furnitures",
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      },
+    },
+  },
+});
