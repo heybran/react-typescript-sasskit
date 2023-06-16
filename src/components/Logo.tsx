0f857910ffd610ad05532f97aea110df5e4308eb
brandon <gwonzhang@gmail.com>
2023-06-16 11:19:41 +0800

    update logo to react svg icon

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,10 +1,16 @@
-import interioLogo from "../assets/interio.svg";
+import reactIcon from "../assets/react.svg";
 import { Link } from "react-router-dom";
 
 export default function Logo() {
   return (
     <Link to="/" className="interio-header__logo">
-      <img src={interioLogo} className="logo" alt="Interio logo" />
+      <div className="interio-header__logo-inner">
+        <img src={reactIcon} alt="React Logo" />
+      </div>
+      <div className="interio-header__logo-text">
+        <span>React + TypeScript</span>
+        <span>SassKit</span>
+      </div>
     </Link>
   );
 }
