ff398c24dcab094dbf4330fd5d60a2de2e57117b
brandon <gwonzhang@gmail.com>
2023-06-18 22:32:34 +0800

    feat: add register form basic functionality (#2)

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,9 +1,16 @@
+import Spinner from "./Spinner";
+import { ChangeEvent, KeyboardEvent, ReactNode } from "react";
+
 interface InputProps {
   id: string;
   labelText: string;
   type?: string;
   name: string;
   placeholder: string;
+  spinner?: boolean;
+  onKeyUp?: (e: KeyboardEvent<HTMLInputElement>) => void;
+  errorMessage?: ReactNode | null;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
 export default function Input({
@@ -12,11 +19,24 @@ export default function Input({
   type = "text",
   name,
   placeholder,
+  spinner,
+  onKeyUp,
+  onChange,
+  errorMessage,
 }: InputProps) {
   return (
     <div className="input-field">
       <label htmlFor={id}>{labelText}</label>
-      <input type={type} id={id} name={name} placeholder={placeholder} />
+      <input
+        type={type}
+        id={id}
+        name={name}
+        placeholder={placeholder}
+        onKeyUp={onKeyUp}
+        onChange={onChange}
+      />
+      {spinner ? <Spinner /> : null}
+      {errorMessage}
     </div>
   );
 }
--- src/routes/Register.tsx
+++ src/routes/Register.tsx
@@ -1,26 +1,107 @@
 import Input from "../components/Input";
-import Button from "../components/Button";
 import { Link } from "react-router-dom";
 import { GitHubLoginButton } from "../components/GithubAuth";
+import { useState, KeyboardEvent, FormEvent } from "react";
+
+type ServerError = {
+  message: string;
+  [key: string]: unknown;
+};
 
 export default function Register() {
+  const [isVerifyingUsername, setIsVerifyingUsername] = useState(false);
+  const [usernameExists, setUsernameExists] = useState(false);
+  const [status, setStatus] = useState("typing");
+  const [user, setUser] = useState({ username: "", password: "" });
+  const [error, setError] = useState<ServerError | null>(null);
+
+  let timeout: ReturnType<typeof setTimeout>;
+  const debounce = (e: KeyboardEvent<HTMLInputElement>) => {
+    const input = e.target as HTMLInputElement;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      setIsVerifyingUsername(true);
+      verifyUsername(input.value.trim());
+    }, 500);
+  };
+
+  const verifyUsername = async (username: string) => {
+    // need to clear out error message
+    setUsernameExists(false);
+    const res = await fetch(`/api/user/${username}`);
+    if (res.ok) {
+      // this user exits in database
+      setUsernameExists(true);
+    } else {
+      setUsernameExists(false);
+    }
+
+    setIsVerifyingUsername(false);
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setStatus("submitting");
+    const formData = new FormData(e.target as HTMLFormElement);
+    const user = Object.fromEntries(formData.entries());
+    try {
+      await fetch("/api/user/create", {
+        method: "POST",
+        body: JSON.stringify(user),
+      });
+      setStatus("success");
+    } catch (error) {
+      setStatus("typing");
+      setError(error as ServerError);
+    }
+  };
+
   return (
     <div className="register">
       <h1>Sign Up</h1>
-      <form action="POST">
+      <form onSubmit={handleSubmit}>
         <Input
           name="username"
           labelText="Username"
           id="register-username"
           placeholder="Your username"
+          spinner={isVerifyingUsername ? true : false}
+          onKeyUp={debounce}
+          onChange={(e) =>
+            setUser({ ...user, username: e.target.value.trim() })
+          }
+          errorMessage={
+            usernameExists ? (
+              <span className="input-field__error">
+                Username already exists.
+              </span>
+            ) : null
+          }
         />
         <Input
+          onChange={(e) =>
+            setUser({ ...user, password: e.target.value.trim() })
+          }
           name="password"
           labelText="Password"
           id="register-password"
           placeholder="Enter a password"
         />
-        <Button text="Complete Sign Up" />
+        <button
+          disabled={
+            isVerifyingUsername ||
+            usernameExists ||
+            status === "submitting" ||
+            user.username === "" ||
+            user.password === ""
+          }
+          className="primary-button"
+          type="submit"
+        >
+          Complete Sign Up
+        </button>
+        {error !== null && <p className="submit-error">{error.message}</p>}
+        <p className="submit-error"></p>
         <footer>
           Already have an account?
           <Link to="/login">Sign in</Link>
