892414b127683cac898b5a1a217875579a7881fe
brandon <gwonzhang@gmail.com>
2023-06-19 14:39:03 +0800

    add // @ts-ignore

--- src/components/UploadWidget.tsx
+++ src/components/UploadWidget.tsx
@@ -1,27 +1,31 @@
 import { useEffect, useRef } from "react";
 
-export default function UploadWidget({ buttonText }) {
+export default function UploadWidget({ buttonText }: { buttonText: string }) {
   const cloudinaryRef = useRef();
   const widgetRef = useRef();
   useEffect(() => {
+    // @ts-ignore
     cloudinaryRef.current = window.cloudinary;
-    widgetRef.current = cloudinaryRef.current.createUploadWidget(
+    // @ts-ignore
+    widgetRef.current = cloudinaryRef.current?.createUploadWidget(
       {
         cloudName: "djwt8gjkl",
         sources: ["local", "url"],
         uploadPreset: "hteepswm",
         folder: "react-typescript-sasskit",
       },
-      (error, result) => {
+      (error: any, result: any) => {
         if (result?.event === "success") {
           const avatarUrl = result.info.secure_url;
           console.log(avatarUrl);
+          console.log(error);
         }
       },
     );
   }, []);
 
   return (
+    // @ts-ignore
     <button type="button" onClick={() => widgetRef.current.open()}>
       {buttonText}
     </button>
