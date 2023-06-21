0ffdbdc02c6094babef4f3a74ccc8f40b3bd4609
brandon <gwonzhang@gmail.com>
2023-06-21 16:22:10 +0800

    change contact support to signout

--- src/components/DashboardNav.tsx
+++ src/components/DashboardNav.tsx
@@ -1,7 +1,18 @@
 import { NavLink } from "react-router-dom";
-import Button from "./Button";
+import { MouseEvent } from "react";
 
 export default function DashboardNav() {
+  const handleSignout = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    fetch("/api/user/signout", {
+      method: "POST",
+      credentials: "include",
+    })
+      .then((res) => res.json())
+      .then(() => (window.location.href = "/"))
+      .catch((error) => console.error(error));
+  };
+
   return (
     <nav className="dashboard-nav">
       <NavLink className="dashboard-nav__link" to="/dashboard/account">
@@ -13,7 +24,9 @@ export default function DashboardNav() {
       {/* <NavLink className="dashboard-nav__link" to="/dashboard/domains">
         Domains
       </NavLink> */}
-      <Button text="Contact Support" />
+      <button className="primary-button relative" onClick={handleSignout}>
+        Sign out
+      </button>
     </nav>
   );
 }
