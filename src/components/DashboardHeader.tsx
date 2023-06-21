97eca59e1ce3463805afc21b8d75f3d21da70465
brandon <gwonzhang@gmail.com>
2023-06-21 16:21:37 +0800

    add header avatar

--- src/components/DashboardHeader.tsx
+++ src/components/DashboardHeader.tsx
@@ -1,8 +1,11 @@
 import Logo from "./Logo";
 import Search from "./Search";
 import { useEffect, useRef } from "react";
+import { useUserContext } from "../context/UserContext";
+import defaultAvatar from "../assets/default_avatar.svg";
 
 export default function DashboardHeader() {
+  const { user } = useUserContext();
   const headerRef = useRef<HTMLElement>(null);
   useEffect(() => {
     const header = headerRef.current as HTMLElement;
@@ -19,6 +22,14 @@ export default function DashboardHeader() {
     >
       <Logo />
       <Search />
+      <button className="interio-header__button-avatar">
+        <img
+          src={user.avatarUrl || defaultAvatar}
+          alt="User Avartar"
+          crossOrigin="anonymous"
+        />
+        <span>{user.username}</span>
+      </button>
     </header>
   );
 }
