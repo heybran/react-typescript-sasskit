b6dcfcc96a1de70a80bf195cc61ba4c3ac9bd99c
brandon <gwonzhang@gmail.com>
2023-06-22 23:14:02 +0800

    update primary button styles and add spinner to register form

--- src/routes/Register.tsx
+++ src/routes/Register.tsx
@@ -2,18 +2,14 @@ import Input from "../components/Input";
 import { Link } from "react-router-dom";
 import { GitHubLoginButton } from "../components/GithubAuth";
 import { useState, KeyboardEvent, FormEvent } from "react";
-
-type ServerError = {
-  message: string;
-  [key: string]: unknown;
-};
+import Spinner from "../components/Spinner";
 
 export default function Register() {
   const [isVerifyingUsername, setIsVerifyingUsername] = useState(false);
   const [usernameExists, setUsernameExists] = useState(false);
   const [status, setStatus] = useState("typing");
   const [user, setUser] = useState({ username: "", password: "" });
-  const [error, setError] = useState<ServerError | null>(null);
+  const [error, setError] = useState<String | null>(null);
 
   let timeout: ReturnType<typeof setTimeout>;
   const debounce = (e: KeyboardEvent<HTMLInputElement>) => {
@@ -45,7 +41,7 @@ export default function Register() {
     const formData = new FormData(e.target as HTMLFormElement);
     const user = Object.fromEntries(formData.entries());
     try {
-      await fetch("/api/user/create", {
+      const res = await fetch("/api/user/create", {
         method: "POST",
         headers: {
           /**
@@ -56,11 +52,17 @@ export default function Register() {
         },
         body: JSON.stringify(user),
       });
-      setStatus("success");
-      window.location.href = "/dashboard/account";
-    } catch (error) {
+
+      if (res.ok) {
+        setStatus("success");
+        window.location.href = "/dashboard/account";
+      } else {
+        const error = await res.json();
+        setError(error.message);
+      }
+    } catch (err: any) {
       setStatus("typing");
-      setError(error as ServerError);
+      setError(err?.message);
     }
   };
 
@@ -110,8 +112,9 @@ export default function Register() {
           type="submit"
         >
           Complete Sign Up
+          {status === "submitting" ? <Spinner /> : null}
         </button>
-        {error !== null && <p className="error">{error.message}</p>}
+        {error !== null && <p className="error">{error}</p>}
         <footer>
           Already have an account?
           <Link to="/login">Sign in</Link>
