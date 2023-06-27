a2d55d573e0b7a1ae5155f2bba35c596df8070f1
brandon <gwonzhang@gmail.com>
2023-06-27 17:41:02 +0800

    add main tag and footer

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function Root() {
   return (
-    <div className="content-wrapper">
+    <div className="main-site">
       <Header />
-      <Outlet />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
     </div>
   );
 }
